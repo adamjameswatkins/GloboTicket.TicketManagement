7923f64ee818fb58ef2909e0e75967edca372e39
Adam Watkins <adamjwatkins@gmail.com>
2021-02-07 23:42:33 +0000

    Add validation
    Added FluentValidation
    Added custom exceptions for notfound badrequest and validation
    Added a CreateEventCommandValidator and used in the CreateEventCommandHandler to validate and throw exception if errors result

---
+++ GloboTicket.TicketManagement.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+
+namespace GloboTicket.TicketManagement.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public List<string> ValidationErrors { get; set; }
+
+        public ValidationException(ValidationResult validationResult)
+        {
+            this.ValidationErrors = new List<string>();
+
+            foreach (var validationError in validationResult.Errors)
+            {
+                this.ValidationErrors.Add(validationError.ErrorMessage);
+            }
+        }
+    }
+}
--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Exceptions;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 using System;
@@ -21,6 +22,14 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
 
         public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
         {
+            var validator = new CreateEventCommandValidator();
+            var validationResult = await validator.ValidateAsync(request);
+
+            if(validationResult.Errors.Count > 0)
+            {
+                throw new ValidationException(validationResult);
+            }
+
             var @event = this.mapper.Map<Event>(request);
 
             @event = await this.eventRepository.AddAsync(@event);
