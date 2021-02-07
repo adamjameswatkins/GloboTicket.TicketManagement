235e4cf18aa80ea9791cd10e26d8b06add000e3a
Adam Watkins <adamjwatkins@gmail.com>
2021-02-07 23:16:08 +0000

    Add Delete Event Command
    Added DeleteEventCommand and DeleteEventCommandHandler

---
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/DeleteEvent/DeleteEventCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.DeleteEvent
+{
+    public class DeleteEventCommandHandler : IRequestHandler<DeleteEventCommand>
+    {
+        private readonly IAsyncRepository<Event> eventRepository;
+
+        public DeleteEventCommandHandler(IAsyncRepository<Event> eventRepository)
+        {
+            this.eventRepository = eventRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteEventCommand request, CancellationToken cancellationToken)
+        {
+            var eventToDelete = await this.eventRepository.GetByIdAsync(request.EventId);
+
+            await this.eventRepository.DeleteAsync(eventToDelete);
+
+            return Unit.Value;
+        }
+    }
+}
