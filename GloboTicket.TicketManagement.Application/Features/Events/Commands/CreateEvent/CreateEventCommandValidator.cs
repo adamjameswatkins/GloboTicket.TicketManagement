82a4e208070a893218d98d370a1ca58b0a669ecb
Adam Watkins <adamjwatkins@gmail.com>
2021-02-07 23:50:54 +0000

    Add validation for unique name and date
    Modified CreateEventCommandValidatoe to check for name and date uniqueness

--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
@@ -1,14 +1,21 @@
 ﻿using FluentValidation;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent
 {
     public class CreateEventCommandValidator : AbstractValidator<CreateEventCommand>
     {
-        public CreateEventCommandValidator()
+        private readonly IEventRepository eventRepository;
+
+        public CreateEventCommandValidator(IEventRepository eventRepository)
         {
+            this.eventRepository = eventRepository;
+         
             RuleFor(p => p.Name)
                 .NotEmpty().WithMessage("{PropertyName} is required.")
                 .NotNull()
@@ -19,9 +26,18 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
                 .NotNull()
                 .GreaterThan(DateTime.Now);
 
+            RuleFor(e => e)
+                .MustAsync(EventNameAndDateUnique)
+                .WithMessage("An event with the same name and date already exists.");
+
             RuleFor(p => p.Price)
                 .NotEmpty().WithMessage("{PropertyName} is required.")
                 .GreaterThan(0);
+        }
+
+        private async Task<bool> EventNameAndDateUnique(CreateEventCommand e, CancellationToken token)
+        {
+            return !(await this.eventRepository.IsEventNameAndDateUnique(e.Name, e.Date));
         }
     }
 }
