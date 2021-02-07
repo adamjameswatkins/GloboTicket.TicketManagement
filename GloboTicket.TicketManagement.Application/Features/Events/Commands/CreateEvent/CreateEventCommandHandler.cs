87776e95ab5e9bd5eb2f26b78aa53d4ffebf050f
Adam Watkins <adamjwatkins@gmail.com>
2021-02-07 23:10:05 +0000

    Add Create  Event Commands
    Added CreateEventCommand and CreateEventCommandHandler
    Updated mapping profile to include new event

---
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent
+{
+    public class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, Guid>
+    {
+        private readonly IMapper mapper;
+        private readonly IEventRepository eventRepository;
+
+        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository)
+        {
+            this.mapper = mapper;
+            this.eventRepository = eventRepository;
+        }
+
+        public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
+        {
+            var @event = this.mapper.Map<Event>(request);
+
+            @event = await this.eventRepository.AddAsync(@event);
+
+            return @event.EventId;
+        }
+    }
+}
--- GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
+++ GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
 using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
 using GloboTicket.TicketManagement.Domain.Entities;
@@ -16,6 +17,7 @@ namespace GloboTicket.TicketManagement.Application.Profiles
             CreateMap<Category, CategoryDto>();
             CreateMap<Category, CategoryListVm>();
             CreateMap<Category, CategoryEventListVm>();
+            CreateMap<Event, CreateEventCommand>().ReverseMap();
         }
     }
 }
