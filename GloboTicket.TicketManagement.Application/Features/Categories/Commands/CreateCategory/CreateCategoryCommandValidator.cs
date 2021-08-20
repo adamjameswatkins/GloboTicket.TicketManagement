58ed0743d07a1d50fbd34f01ce6fae1a9224e864
AdamJamesWatkins <adamjwatkins@gmail.com>
2021-08-20 20:29:57 +0100

    Add validation for CreateCategoryCommand

--- GloboTicket.TicketManagement.Application/Features/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
+++ GloboTicket.TicketManagement.Application/Features/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -2,7 +2,14 @@
 
 namespace GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCategory
 {
-    class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+    public class CreateCategoryCommandValidator: AbstractValidator<CreateCategoryCommand>
     {
+        public CreateCategoryCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 10 characters.");
+        }
     }
 }
