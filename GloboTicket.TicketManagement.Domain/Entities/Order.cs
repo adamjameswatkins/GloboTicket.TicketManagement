60b18ad010b07b1e5030fa3dd42f32c2f17da689
AdamJamesWatkins <adamjwatkins@gmail.com>
2021-08-20 20:29:08 +0100

    Fix OrderTotal name

--- GloboTicket.TicketManagement.Domain/Entities/Order.cs
+++ GloboTicket.TicketManagement.Domain/Entities/Order.cs
@@ -7,7 +7,7 @@ namespace GloboTicket.TicketManagement.Domain.Entities
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
-        public int OderTotal { get; set; }
+        public int OrderTotal { get; set; }
         public DateTime OrderPlaced { get; set; }
         public bool OrderPaid { get; set; }
     }
