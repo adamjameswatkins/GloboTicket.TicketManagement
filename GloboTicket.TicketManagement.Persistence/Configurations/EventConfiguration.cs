f88c8fce04289af699b1d55ce213e593762c8cef
AdamJamesWatkins <adamjwatkins@gmail.com>
2021-08-20 20:54:13 +0100

    Add configuration for Events

---
+++ GloboTicket.TicketManagement.Persistence/Configurations/EventConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GloboTicket.TicketManagement.Persistence.Configurations
+{
+    public class EventConfiguration
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
