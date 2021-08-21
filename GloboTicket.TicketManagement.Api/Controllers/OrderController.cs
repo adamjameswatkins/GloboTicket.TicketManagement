95458155268f45d40eac2b7f64d66e13b06fc44c
AdamJamesWatkins <adamjwatkins@gmail.com>
2021-08-21 15:59:56 +0100

    Add Order and Events controllers
    OrderController added with no actions
    EventsController added with Get, GetById, Create, Update, Delete

---
+++ GloboTicket.TicketManagement.Api/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GloboTicket.TicketManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+    }
+}
