4b462d1fde88c87e244f14c53440de00b9173e72
AdamJamesWatkins <adamjwatkins@gmail.com>
2021-08-21 16:58:12 +0100

    Add Swagger support
    Added Swashbuckle and Swashbuckler Swagger
    Added OperationFilter needed for the Csv action to be supported in Swagger

--- GloboTicket.TicketManagement.Api/Startup.cs
+++ GloboTicket.TicketManagement.Api/Startup.cs
@@ -1,3 +1,4 @@
+using GloboTicket.TicketManagement.Api.Utility;
 using GloboTicket.TicketManagement.Application;
 using GloboTicket.TicketManagement.Infrastructure;
 using GloboTicket.TicketManagement.Persistence;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +29,8 @@ namespace GloboTicket.TicketManagement.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            AddSwagger(services);
+
             services.AddApplicationServices();
             services.AddInfrastructureServices(Configuration);
             services.AddPersistenceServices(Configuration);
@@ -35,6 +39,20 @@ namespace GloboTicket.TicketManagement.Api
             services.AddCors(options =>
             {
                 options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "GloboTicket Ticket Mangement API"
+                });
+
+                c.OperationFilter<FileResultContentTypeOperationFilter>();
             });
         }
 
@@ -48,6 +66,12 @@ namespace GloboTicket.TicketManagement.Api
 
             app.UseHttpsRedirection();
             app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "GloboTicket Ticket Management API");
+            });
 
             app.UseCors("Open");
 
