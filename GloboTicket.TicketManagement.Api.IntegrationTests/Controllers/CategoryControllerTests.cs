7198cf7d5303e726f0366b496d421ed2dd3d5052
AdamJamesWatkins <adamjwatkins@gmail.com>
2021-08-21 23:53:18 +0100

    Add Api Integration test
    Added test using WebApplicationFactory

---
+++ GloboTicket.TicketManagement.Api.IntegrationTests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using GloboTicket.TicketManagement.API.IntegrationTests.Base;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GloboTicket.TicketManagement.Api.IntegrationTests
+{
+    public class CategoryControllerTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> factory;
+
+        public CategoryControllerTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public async Task ReturnsSuccessResult()
+        {
+            var client = this.factory.GetAnonymousClient();
+
+            var response = await client.GetAsync("api/category/all");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<List<CategoryListVm>>(responseString);
+
+            Assert.IsType<List<CategoryListVm>>(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
