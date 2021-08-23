88b6eca92a01360b4a60e5ff9d456d1f3316cef5
AdamJamesWatkins <adamjwatkins@gmail.com>
2021-08-23 15:55:07 +0100

    Added the code for allowing paged orders in the client

--- GloboTicket.TicketManagement.Api/Controllers/OrderController.cs
+++ GloboTicket.TicketManagement.Api/Controllers/OrderController.cs
@@ -1,5 +1,9 @@
-﻿using MediatR;
+﻿using GloboTicket.TicketManagement.Application.Features.Orders.GetOrdersForMonth;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace GloboTicket.TicketManagement.Api.Controllers
 {
@@ -12,6 +16,17 @@ namespace GloboTicket.TicketManagement.Api.Controllers
         public OrderController(IMediator mediator)
         {
             this.mediator = mediator;
+        }
+
+        [HttpGet("/getpagedordersformonth", Name = "GetPagedOrdersForMonth")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<PagedOrdersForMonthVm>> GetPagedOrderForMonth(DateTime date, int page, int size)
+        {
+            var getOrdersForMonthQuery = new GetOrdersForMonthQuery() { Date = date, Page = page, Size = size };
+            var dtos = await this.mediator.Send(getOrdersForMonthQuery);
+
+            return Ok(dtos);
         }
     }
 }
